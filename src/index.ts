11fabd6715f20356d89ff4f8038e4ec996ac2a20
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2021-09-10 19:33:01 +0200

    Added Server Implementation

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+const TOKEN = process.env.TOKEN;
+
+app.get('/players/:tag', (req, res) => {
+  console.log(`From: ${req.ip}\t\t\tPath: ${req.url}`);
+
+  const before = new Date().getTime();
+
+  axios
+    .get(`https://api.brawlstars.com/v1/players/${encodeURIComponent('#' + req.params.tag)}`, {
+      headers: { accept: 'application/json', authorization: `Bearer ${TOKEN}` },
+    })
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+
+  const after = new Date().getTime();
+
+  console.log(`Duration: ${after - before} ms\n`);
+});
+
+app.listen(port, () => {});
+
+console.log('Server Started!');
